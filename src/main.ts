77114c508ed549939298eee7d194a53f78068d45
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-10-05 16:27:06 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,12 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+let num_clicks = 0;
+const button = document.createElement("button");
+button.innerHTML = "I'm button";
+button.onclick = () => {
+    num_clicks++;
+    button.innerHTML = `Buttoned (${num_clicks}) Times`;
+}
+app.append(button);
