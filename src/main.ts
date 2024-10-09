133283d22385ab5daa916bd3ba77c7d89e82825f
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-10-09 14:34:34 -0700

    Step 9 Complete

--- src/main.ts
+++ src/main.ts
@@ -27,10 +27,6 @@ const growthR = document.createElement("div");
 growthR.innerHTML = `Current Growth Rate: ${growthRate}`;
 app.append(growthR);
 
-/*const upgradeButton = document.createElement("button");
-upgradeButton.innerHTML = "Buy Automatic Clicking (10 clicks)";
-upgradeButton.disabled = true;
-app.append(upgradeButton);*/
 interface shopButton {
   rate: number;
   price: number;
@@ -38,21 +34,7 @@ interface shopButton {
   fName: string;
   button: HTMLButtonElement;
 }
-/*const upgradeButton: shopButton = createShopButton(
-  10,
-  0.1,
-  `Upgrade Alien Farms (10.00 clicks for 0.1 aliens/sec)(Bought 0 Times)`
-);
-const upgradeButton2: shopButton = createShopButton(
-  100,
-  2.0,
-  `Upgrade Alien Breeder (100.00 clicks for 2 aliens/sec)(Bought 0 Times)`
-);
-const upgradeButton3: shopButton = createShopButton(
-  1000,
-  50.0,
-  `Upgrade Alien Duplicator (1000.00 clicks for 50 aliens/sec)(Bought 0 Times)`
-);*/
+
 const availableItems: shopButton[] = [
   { purchased: 0, price: 10, rate: 0.1, fName: "Alien Farm", button },
   { purchased: 0, price: 100, rate: 2, fName: "Alien Breeder", button },
@@ -60,15 +42,7 @@ const availableItems: shopButton[] = [
 ];
 let lastTime: number;
 let clickedUpgrade: boolean = false;
-/*upgradeButton.onclick = () => {
-  if (num_clicks >= 10) {
-    num_clicks -= 10;
-    clickedUpgrade = true;
-    growthRate++;
-    upgradeButton.innerHTML = "Upgrade Automatic Clicking (10 clicks)";
-    requestAnimationFrame(autoClick);
-  }
-};*/
+
 function autoClick(timestamp: number) {
   if (clickedUpgrade) {
     lastTime = timestamp;
@@ -109,46 +83,4 @@ function buttonUpdate() {
       availableItems[b].button.disabled = true;
     }
   }
-}
-/*upgradeButton.button.innerHTML = `Upgrade Alien Farms (${upgradeButton.price.toFixed(2)} clicks for 0.1 aliens/sec)(Bought ${upgradeButton.purchased} Times)`;
-  upgradeButton2.button.innerHTML = `Upgrade Alien Breeder (${upgradeButton2.price.toFixed(2)} clicks for 2 aliens/sec)(Bought ${upgradeButton2.purchased} Times)`;
-  upgradeButton3.button.innerHTML = `Upgrade Alien Duplicator (${upgradeButton3.price.toFixed(2)} clicks for 50 aliens/sec)(Bought ${upgradeButton3.purchased} Times)`;
-  if (num_clicks >= upgradeButton.price) {
-    upgradeButton.button.disabled = false;
-
-    if (num_clicks >= upgradeButton2.price) {
-      upgradeButton2.button.disabled = false;
-    }
-    if (num_clicks >= upgradeButton3.price) {
-      upgradeButton3.button.disabled = false;
-    }
-  } else {
-    upgradeButton.button.disabled = true;
-    upgradeButton2.button.disabled = true;
-    upgradeButton3.button.disabled = true;
-  }
-}*/
-
-function createShopButton(cost: number, growth: number, text: string) {
-  const button = document.createElement("button");
-  button.innerHTML = text;
-  button.disabled = true;
-  const count = 0;
-  app.append(button);
-  const shopButt = {} as shopButton;
-  shopButt.button = button;
-  shopButt.purchased = count;
-  shopButt.price = cost;
-  shopButt.text = text;
-  button.onclick = () => {
-    if (num_clicks >= shopButt.price) {
-      num_clicks -= shopButt.price;
-      shopButt.price = shopButt.price * 1.15;
-      clickedUpgrade = true;
-      growthRate += growth;
-      shopButt.purchased++;
-      requestAnimationFrame(autoClick);
-    }
-  };
-  return shopButt;
 }
