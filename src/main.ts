3225b76ab7dab2bb64e29f263dc5dc770b83bc32
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-10-25 23:05:47 -0700

    App Revision, Starry Background, Alien added

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ for (let i = 0; i < 100; i++) {
   star.style.height = `${size}px`;
   star.style.left = `${Math.random() * 100}vw`;
   star.style.top = `${Math.random() * 100}vh`;
-  container.appendChild(star);
+  container?.appendChild(star);
 }
 
 const gameName = "👾";
